22620f1b4e80a91f789b911fb39d75a1dfe2aeca
Roger <rtgx2000@gmail.com>
2025-08-04 13:40:42 +0800

    first commit

--- App.tsx
+++ App.tsx
@@ -1,28 +1,41 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList } from 'react-native';
+import axios from 'axios';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+type User = {
+  id: number;
+  name: string;
+  createdAt: string;
+};
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    axios.get<User[]>('http://10.0.2.2:8080/users')
+      .then(res => setUsers(res.data))
+      .catch(err => setError('Error: ' + err.message));
+  }, []);
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {error ? (
+        <Text>{error}</Text>
+      ) : (
+        <FlatList
+          data={users}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({ item }) => (
+            <View style={{ margin: 10 }}>
+              <Text>User: {item.name}</Text>
+              <Text>Created At: {item.createdAt}</Text>
+            </View>
+          )}
+        />
+      )}
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+};
 
 export default App;
